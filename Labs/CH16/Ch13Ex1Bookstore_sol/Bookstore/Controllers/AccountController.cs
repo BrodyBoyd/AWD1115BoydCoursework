12a7b3f19db43fe3e466d800546afbf7b0337a93
BrodyBoyd <Brody_Boyd@insideranken.org>
2026-04-06 16:20:36 -0500

    Ch16 Lab

---
+++ Labs/CH16/Ch13Ex1Bookstore_sol/Bookstore/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Bookstore.Models.DomainModels;
+using Bookstore.Models.ViewModels;
+
+namespace Bookstore.Controllers
+{
+    public class AccountController(UserManager<User> _userManager, SignInManager<User> _signInManager) : Controller
+    {
+        private UserManager<User> userManager = _userManager;
+        private SignInManager<User> signInManager = _signInManager;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.Username };
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "")
+        {
+            var model = new LoginViewModel { ReturnUrl = returnUrl };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await signInManager.PasswordSignInAsync(model.Username, model.Password, isPersistent:model.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid login attempt.");
+
+            return View(model);
+        }
+    }
+}
---
+++ Labs/CH16/Ch13Ex1Bookstore_sol/Bookstore/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Please endter a UserName")]
+        [StringLength(255)]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please endter a Password")]
+        [DataType(DataType.Password)]
+        [Compare("ConfirmPassword", ErrorMessage = "Passwords do not match")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please endter a Confirm Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Conrim Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
